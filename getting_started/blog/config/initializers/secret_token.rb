40f43239b4f000de1b49b3c32b054a65ec6f30af
Borislav Roussev <borislav@udacity.com>
2013-07-28 10:14:27 -0700

    shopping cart steps

---
+++ getting_started/blog/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = '6670c6a2dd9895b45940f50228678119a6cc7a3109ef62b62d6f78942ad34a0e4ed3681926e46033c672a96da74c03ad4819d0a005f90131f5334cf74fea75b5'
